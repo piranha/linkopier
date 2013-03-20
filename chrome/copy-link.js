eefb2e87581c69e8c21a9fdf1cf5124b935c5c87
Alexander Solovyov <alexander@solovyov.net>
2013-03-20 12:04:44 +0200

    make it work under new chrome

--- chrome/copy-link.js
+++ chrome/copy-link.js
@@ -1,37 +1,11 @@
-var coord = {x: 0, y: 0};
-
-document.addEventListener('mousemove', function(e) {
-    coord.x = e.clientX;
-    coord.y = e.clientY;
-});
-
-function getLink() {
-    var el = document.elementFromPoint(coord.x, coord.y);
-    var link;
-    switch (el.tagName) {
-    case 'A':
-        return el.href;
-    case 'IMG':
-        return el.src;
-    default:
-        return '';
-    }
-}
-
-if (window == top) {
-    window.addEventListener('keyup', keyListener, false);
-}
-
-// ctrl-a/ctrl-s
-function keyListener(e) {
-    if (!e.ctrlKey || e.metaKey || e.shiftKey || e.altKey)
+function onRequest(link) {
+    var input = document.getElementById('url');
+    if (!input)
         return;
-    var url;
-    if (e.keyCode == 65) // 'a'
-        url = window.location.href;
-    else if (e.keyCode == 83) // 's'
-        url = getLink();
-    else
-        return;
-    chrome.extension.sendRequest(url);
+
+    input.value = link;
+    input.select();
+    document.execCommand('copy', false, null);
 }
+
+chrome.extension.onMessage.addListener(onRequest);
